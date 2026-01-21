9e88054de3fd58499fa86f7e4cbaf65a9634943b
fanluojun <1412283473@qq.com>
2026-01-21 20:43:22 +0800

    finish basic lab

--- lab7/bstmap/BSTMap.java
+++ lab7/bstmap/BSTMap.java
@@ -3,63 +3,115 @@ package bstmap;
 import java.util.Iterator;
 import java.util.Set;
 
-public class BSTMap<K, V> implements Map61B<K, V> {
+public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
+    private Node root;
+    private int size;
+
     private class Node {
-        private int size;
         private Node left;
         private Node right;
-        private K key;
+        private final K key;
         private V value;
 
-        public Node(K key, V value, int size) {
-            this.size = size;
+        public Node(K key, V value) {
             this.key = key;
             this.value = value;
         }
     }
 
+    public BSTMap() {
+        root = null;
+        size = 0;
+    }
+
     @Override
     public void clear() {
-
+        root = null;
+        size = 0;
     }
 
     @Override
     public boolean containsKey(K key) {
-        return false;
+        return getKey(key, root) != null;
     }
 
     @Override
     public V get(K key) {
-        return null;
+        return get(key, root);
+    }
+
+    private V get(K key, Node n) {
+        if (n == null) {
+            return null;
+        }
+        int cmp = n.key.compareTo(key);
+        if (cmp > 0) {
+            return get(key, n.left);
+        }
+        if (cmp < 0) {
+            return get(key, n.right);
+        }
+        return n.value;
+    }
+
+    private V getKey(K key, Node n) {
+        if (n == null) {
+            return null;
+        }
+        int cmp = n.key.compareTo(key);
+        if (cmp > 0) {
+            return get(key, n.left);
+        }
+        if (cmp < 0) {
+            return get(key, n.right);
+        }
+        return (V) n;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public void put(K key, V value) {
+        root = put(key, value, root);
+    }
 
+    private Node put(K key, V value, Node n) {
+        if (n == null) {
+            size += 1;
+            return new Node(key, value);
+        }
+        int cmp = n.key.compareTo(key);
+        if (cmp == 0) {
+            n.value = value;
+        } else if (cmp > 0) {
+            n.left = put(key, value, n.left);
+        } else {
+            n.right = put(key, value, n.right);
+        }
+        return n;
     }
 
     @Override
     public Set<K> keySet() {
-        return Set.of();
+        throw new UnsupportedOperationException();
+//        return Set.of();
     }
 
     @Override
     public V remove(K key) {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public V remove(K key, V value) {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public Iterator<K> iterator() {
-        return null;
+        throw new UnsupportedOperationException();
     }
 }
